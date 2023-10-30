e6cdd2d38bb2bf6b242193d29300f974c1e9ba07
Vonique Stricklen <vstrickl.git@gmail.com>
2023-10-29 19:11:55 -0700

    Added a CSRF_TRUSTED_ORIGIN

--- vonResume/settings.py
+++ vonResume/settings.py
@@ -141,4 +141,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # CSRF Tokens
 # https://docs.djangoproject.com/en/4.2/howto/csrf/
 
-CSRF_TRUSTED_ORIGINS = ['https://vonstrick.com','http://127.0.0.1:8000']
+CSRF_TRUSTED_ORIGINS = ['https://vonstrick.com','http://127.0.0.1:8000', 'https://vonstrick-production.up.railway.app']
