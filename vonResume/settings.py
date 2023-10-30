927ca05a1ab10f224896a550ac481b3ad7cc0ff8
Vonique Stricklen <vstrickl.git@gmail.com>
2023-10-30 12:52:20 -0700

    Updated ALLOWED_HOSTS

--- vonResume/settings.py
+++ vonResume/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='').split(',')
+ALLOWED_HOSTS = ['.vonstrick.com', '127.0.0.1']
 
 
 # Application definition
